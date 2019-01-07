074dc7f27fec046c900fae4387afa36d398d92b7
infinity1096 <2315465052@qq.com>
2019-01-07 21:41:47 +0800

    Chassis update
    reserved code for real chassis

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -14,5 +14,14 @@ package frc.robot;
  * floating around.
  */
 public class RobotMap {
-  public static final int TESTING_MOTOR_PORT = 1;
+  //motors
+    //testing
+    public static final int TESTING_MOTOR_PORT = 1;
+    //chassis
+    public static final int LF_MOTOR_PORT = 1;
+    public static final int LM_MOTOR_PORT = 2;
+    public static final int LR_MOTOR_PORT = 3;
+    public static final int RF_MOTOR_PORT = 4;
+    public static final int RM_MOTOR_PORT = 5;
+    public static final int RR_MOTOR_PORT = 6;
 }
--- src/main/java/frc/robot/subsystems/Chassis.java
+++ src/main/java/frc/robot/subsystems/Chassis.java
@@ -8,10 +8,10 @@
 package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
 import com.ctre.phoenix.motorcontrol.can.TalonSRX;
 
-import org.apache.commons.math3.linear.MatrixUtils;
-import org.apache.commons.math3.linear.RealMatrix;
+import org.apache.commons.math3.filter.KalmanFilter;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 import frc.robot.RobotMap;
@@ -22,12 +22,51 @@ import frc.robot.commands.DefaultDrive;
  */
 public class Chassis extends Subsystem {
 
-  public Chassis(){}
-
   private TalonSRX testmotor = new TalonSRX(RobotMap.TESTING_MOTOR_PORT);
+  /*
+  private TalonSRX lf = new TalonSRX(RobotMap.LF_MOTOR_PORT);
+  private TalonSRX lm = new TalonSRX(RobotMap.LM_MOTOR_PORT);
+  private TalonSRX lr = new TalonSRX(RobotMap.LR_MOTOR_PORT);
+  private TalonSRX rf = new TalonSRX(RobotMap.RF_MOTOR_PORT);
+  private TalonSRX rm = new TalonSRX(RobotMap.RM_MOTOR_PORT);
+  private TalonSRX rr = new TalonSRX(RobotMap.RR_MOTOR_PORT);
+  */
+  public Chassis(){
+    testmotor.setSensorPhase(false);
+    testmotor.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder,0,30);
+    //testmotor.setSelectedSensorPosition(0, 0, 10);
+    /*
+    lm.follow(lf);
+    lr.follow(lf);
+    rm.follow(rf);
+    rr.follow(rf);
+    */
+  }
+
+
+  
+
+  //measurments
+  /*
+  public double[] getCurrent(){
+    double[] results = {0,0};
+    results[0] = lf.getOutputCurrent() + lm.getOutputCurrent() + lr.getOutputCurrent();
+    results[1] = rf.getOutputCurrent() + rm.getOutputCurrent() + rr.getOutputCurrent();
+    return results;
+  }
+  
+  public double[][] getState(){
+    double [][] results = {{0,0},{0,0}};
+    //results [0,:] = Position, [1,:] = velocity.
+    //TODO
+  }
+  */
+
+  
 
   public void drive(double input){
-    testmotor.set(ControlMode.PercentOutput, input);
+    //testmotor.set(ControlMode.PercentOutput, input);
+    testmotor.set(ControlMode.Position, 300);
   }
 
   public double getP(){
