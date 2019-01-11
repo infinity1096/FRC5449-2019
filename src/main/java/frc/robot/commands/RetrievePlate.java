c47fdc94bee2343973a02ee76eee0cc9b420b6fa
TRG-Scorpion <40878138+TRG-Scorpion@users.noreply.github.com>
2019-01-11 16:02:16 +0800

    Plate Dispenser Revised
    -Spell check

--- src/main/java/frc/robot/commands/RetrievePlate.java
+++ src/main/java/frc/robot/commands/RetrievePlate.java
@@ -10,8 +10,8 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
-public class RetrivePlate extends Command {
-  public RetrivePlate() {
+public class RetrievePlate extends Command {
+  public RetrievePlate() {
     requires(Robot.platedispenser);
   }
 
